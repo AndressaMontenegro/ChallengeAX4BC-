f6c22fb2b0e1e35c5fa6e97dd89caf2224651ffd
Andressa Montenegro <montenegroandresa@outlook.com>
2020-10-14 15:41:36 -0300

    Projeto com APIClientRegister pronta

---
+++ ApiClientsRegister/Controllers/ClientsController.cs
@@ -0,0 +1,31 @@
+﻿using ApiClientsRegister.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiClientsRegister.Controllers
+{
+    public class ClientsController : ApiController
+    {
+        private static List<Client> clients = new List<Client>();
+
+        public List<Client> Get()
+        {
+            return clients;
+        }
+
+        public void Post (string name, string email, string address, int idClient)
+        {
+            if (idClient != 0)
+                clients.Add(new Client(name, email, address, idClient));
+        }
+
+        public void Delete (string name, string email, string address, int idClient)
+        {
+            clients.RemoveAt(clients.IndexOf(clients.First(x => x.IdClient.Equals(idClient))));
+        }
+    }
+}
---
+++ ApiClientsRegister/Models/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiClientsRegister.Models
+{
+    public class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public int IdClient { get; set; }
+
+        public Client (string name, string email, string address, int idClient)
+        {
+            this.Name = name;
+            this.Email = email;
+            this.Address = address;
+            IdClient = idClient++;
+        }
+            
+
+    }
+}
